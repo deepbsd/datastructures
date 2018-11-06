7273458fd56aead1edcc8a911c1c958fdaeac29f
David S. Jackson <deepbsd@yahoo.com>
2018-11-06 10:37:13 -0500

    doing linkedListFunctions exercises

--- linkedListsFunctions.js
+++ linkedListsFunctions.js
@@ -8,12 +8,23 @@ function findMiddle(linkedList){
         node = node.next;
         count++;
     }
-    for (let i = count; i <= Math.round(count/2); i--){
-        node = node.prev;
+    node = linkedList.head;
+    for (let i = 0; i <= Math.round(count/2); i++){
+        node = node.next;
     }
     return node;
 }
 
+function findMiddle1(linkedList){
+    let slow = linkedList.head;
+    let fast = linkedList.head;
+
+    while (fast.next !== null && fast !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
 
 function find3rdFromEnd(list){
     let node = linkedList.head;
@@ -22,26 +33,58 @@ function find3rdFromEnd(list){
         node = node.next;
         count++;
     }
-    for (let i = count; i <= count-3; i--){
-        node = node.prev;
+    let limit = count-3;
+    let node = linkedList.head;
+    for (let i = 0; i <= limit; i++){
+        node = node.next;
     }
     return node;
 }
 
+function find3rdFromEnd1(list){
+    let node = list.head;
+    let slow = list.head;
+    let count = 0;
+    while (node !== null){
+        if (count >= 2){
+            slow = slow.next;
+        }
+        node = node.next;
+        count++;
+    }
+    return slow;
+}
 
 function reverseLinkedList(linkedList){
-  let LL = new LinkedList();
-  let node = null;
-  for (let i=0; i<linkedList.length-1; i++){
-      node = node.next;
-  }
-  // node should now point to the last node in linkedList
-  for (let i=0; i<linkedList.length; i++){
-      LL.insert(i, node);
-      node = linkedList.prev;
-  }
-  return LL;
+    let prevptr = null, currptr = linkedList.head; 
+    let nextptr = currptr.next;
+    
+    while (currptr !== null){
+        // step 1 is to increment the three pointers, to 
+        // move them to the right so to speak
+        prevptr = currptr; nextptr = currptr.next.next; currptr = currptr.next;
+
+        // step 2 is to make the pointers "rotate" so the next becomes
+        // the previous and so on
+        currptr.next = null; nextptr.next = currptr; 
+    }
+    linkedList.head = currptr;
+    return linkedList;
 }
+
+//function reverseLinkedList(linkedList){
+//  let LL = new LinkedList();
+//  let node = linkedList.head;
+//  for (let i=0; i<linkedList.length-1; i++){
+//      node = node.next;
+//  }
+//  // node should now point to the last node in linkedList
+//  for (let i=0; i<linkedList.length; i++){
+//      LL.insert(i, node);
+//      node = linkedList.prev;
+//  }
+//  return LL;
+//}
 
 
 // This functions assume I can write to the linked list
@@ -49,11 +92,11 @@ function reverseLinkedList(linkedList){
 // There's also the fast and slow pointer approach to 
 // finding a cycle
 function hasCycle(linkedList){
-    let node = null;
+    let node = linkedList.head;
     for (let i=0; i<linkedList.length; i++){
-        node = linkedList.next;
         if (node.visited){ return true; }
         node.visited = true;
+        node = node.next;
     }
     return false;
 }
@@ -70,3 +113,5 @@ function hasCycle1(linkedList){
     }
     return false;
 }
+
+module.exports = { hasCycle1, hasCycle, findMiddle, findMiddle1, find3rdFromEnd, find3rdFromEnd1, reverseLinkedList }
